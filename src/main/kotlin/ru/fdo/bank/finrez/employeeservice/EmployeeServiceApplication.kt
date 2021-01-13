0dbdaae0ad503a52c0a736cbf2d0cc9cbe9f4d85
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-13 18:17:05 +0300

    added @EnableEurekaClient

--- src/main/kotlin/ru/fdo/bank/finrez/employeeservice/EmployeeServiceApplication.kt
+++ src/main/kotlin/ru/fdo/bank/finrez/employeeservice/EmployeeServiceApplication.kt
@@ -2,8 +2,10 @@ package ru.fdo.bank.finrez.employeeservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 open class EmployeeServiceApplication
 
 fun main(args: Array<String>) {
