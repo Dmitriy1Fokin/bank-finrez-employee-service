fcad65e62f16b94e89f66764085601eddf464028
Dmitrii Fokin <fokinds1@gmail.com>
2021-01-13 13:36:15 +0300

    first commit. Init empty project

---
+++ src/main/kotlin/ru/fdo/bank/finrez/employeeservice/EmployeeServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.fdo.bank.finrez.employeeservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmployeeServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmployeeServiceApplication>(*args)
+}
